3b7f2ffd9c0dfd0c0bb0e8243778d6e36ed1b896
mhmdkoeik <44830755+mhmdkoeik@users.noreply.github.com>
2020-03-26 22:02:20 +0200

    Add project files.

---
+++ Assignment-1/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment_1.Models;
+
+namespace Assignment_1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyStudentName()
+        {
+
+            Student st = new Student();
+            st.student_name = "Mohammad Koeik";
+            return View(st);
+        }
+    }
+}
---
+++ Assignment-1/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_1.Models
+{
+    public class Student
+    {
+        public  int student_id { get; set; }
+
+        public  string student_name { get; set; }
+    }
+}
